b43dc8afa3aed5e4a732cc284b9a25c8341e9ade
orifjon <orifjon.5000@gmail.com>
2021-12-24 23:55:42 +0900

    add false

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function List({ user, onDelete }) {
-  const [isInfo, setIsInfo] = useState(true);
+  const [isInfo, setIsInfo] = useState(false);
   const setInfo = () => {
     setIsInfo(!isInfo);
   };
